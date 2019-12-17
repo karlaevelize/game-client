69a5306f2288b75b7f16332c80137046fd9be6e4
karaevelize <k.evelize@gmail.com>
2019-12-17 12:18:16 +0100

    Form is working, it displays the action

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import superagent from "superagent";
+import "./App.css";
 import Homepage from "./components/Homepage";
 import Toolbar from "./components/Toolbar";
 import SignUpPage from "./components/SignUpPage";
@@ -10,18 +11,45 @@ class App extends Component {
   url = "http://localhost:4000";
   stream = new EventSource(`${this.url}/stream`);
 
+  state = {
+    text: ""
+  };
+
   componentDidMount() {
     this.stream.onmessage = event => {
       const { data } = event;
       const action = JSON.parse(data);
-      console.log("test", action);
+      console.log("test action", action);
     };
   }
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const response = await superagent
+        .post(`${this.url}/gameroom`)
+        .send({ name: this.state.text });
+      console.log("response test", response);
+    } catch (error) {
+      console.warn("error test", error);
+    }
+  };
+
+  onChange = event => {
+    const { value } = event.target;
+    this.setState({ text: value });
+  };
 
   render() {
     return (
       <div className="App">
         <Toolbar />
+        <br />
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} value={this.state.text} type="text" />
+          <button>Submit</button>
+        </form>
         <Switch>
           <Route path="/login" component={LoginPage} />
           <Route path="/signup" component={SignUpPage} />
